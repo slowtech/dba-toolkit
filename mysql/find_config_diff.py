22f581eb7930d0d8ea3230735a12668bd93f738d
slowtech <slowtech@qq.com>
2026-05-06 03:45:17 +0000

    optimize find_config_diff.py

--- mysql/find_config_diff.py
+++ mysql/find_config_diff.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
-import MySQLdb,re,prettytable,optparse,os,sys,subprocess,tempfile
+import pymysql,re,prettytable,optparse,os,sys,subprocess,tempfile
+
+no_need_to_check_variables = ['admin_address', 'admin_port', 'admin_ssl_ca','admin_ssl_cert','admin_ssl_key','basedir','build_id','caching_sha2_password_private_key_path','caching_sha2_password_public_key_path','tmpdir','thread_handling','temptable_max_mmap', 'temptable_max_ram', 'temptable_use_mmap','general_log_file','table_encryption_privilege_check','slave_load_tmpdir','server_id','server_uuid','replica_load_tmpdir','sha256_password_private_key_path','sha256_password_public_key_path','persist_only_admin_x509_subject','hostname','log_error','character_sets_dir','slow_query_log', 'slow_query_log_file','relay_log','relay_log_basename','relay_log_index','innodb_undo_directory','secure_file_priv','datadir','socket','replication_optimize_for_static_plugin_config','pid_file','plugin_dir','mandatory_roles','innodb_ft_result_cache_limit','gtid_purged','lc_messages_dir','group_replication_consistency','innodb_use_fdatasync','binlog_rows_query_log_events','log_error_services','log_error_services','innodb_buffer_pool_in_core_file','innodb_buffer_pool_dump_at_shutdown','innodb_buffer_pool_load_at_startup','gtid_executed','innodb_temp_data_file_path','innodb_tmpdir','innodb_undo_tablespaces','innodb_use_native_aio','innodb_monitor_enable','innodb_doublewrite','innodb_doublewrite_files','innodb_doublewrite_pages','innodb_checksum_algorithm', 'log_bin_basename','log_error_suppression_list','log_output', 'rpl_read_size','thread_stack','bind_address','binlog_expire_logs_auto_purge','license','innodb_buffer_pool_chunk_size','innodb_data_home_dir','relay_log_recovery','innodb_log_group_home_dir','innodb_temp_tablespaces_dir','innodb_version','max_binlog_size', 'binlog_cache_size','innodb_page_cleaners','key_buffer_size']
 
 def get_variables(config_file):
     variables = {}
@@ -38,7 +40,7 @@ def get_variables(config_file):
 
 def get_variables_from_instance(host,port,user,passwd):
     try:
-        conn=MySQLdb.connect(host=host,port=port,user=user,passwd=passwd)
+        conn=pymysql.connect(host=host,port=port,user=user,passwd=passwd)
         cursor = conn.cursor()
         query='show global variables'
         cursor.execute(query)
@@ -46,7 +48,7 @@ def get_variables_from_instance(host,port,user,passwd):
         variables=dict(results)
         return variables
     except Exception as e:
-        print e
+        print(e)
 
 def convert_variable_value(variable_value):
     #路径区分大小写，所以路径直接返回
@@ -66,7 +68,7 @@ def convert_variable_value(variable_value):
 
 def convert_variable_name(variables):
     convert_variables={}
-    for variable_name,variable_value in variables.iteritems():
+    for variable_name,variable_value in variables.items():
         new_variable_name=variable_name.replace('-','_')
         new_variable_name=new_variable_name.strip()
         convert_variables[new_variable_name]=variable_value
@@ -163,19 +165,22 @@ def main():
     pt.align='l'
     pt.padding_width = 1  # One space between column edges and contents (default)
     pt.max_width=40
+    """
     for each_variable in sorted(common_variables):
         if convert_variable_value(convert_variables_one[each_variable]) == convert_variable_value(convert_variables_two[each_variable]):
             pt.add_row([each_variable,convert_variables_one[each_variable],convert_variables_two[each_variable]])
-    row = ''.join(['-' for num in xrange(10)])
+    row = ''.join(['-' for num in range(10)])
     pt.add_row([row,row,row])
-    
+    """
     for each_variable in sorted(common_variables):
+        if each_variable in no_need_to_check_variables or 'performance_schema_' in each_variable or 'ssl_' in each_variable:
+            continue
         if convert_variable_value(convert_variables_one[each_variable]) != convert_variable_value(convert_variables_two[each_variable]):
             pt.add_row([each_variable,convert_variables_one[each_variable],convert_variables_two[each_variable]])
             # print each_variable.ljust(25),convert_variables_one[each_variable].ljust(25),convert_variables_two[each_variable].ljust(25)
     if config_file_one and config_file_two:
         variables_one_only=set_variables_one - set_variables_two
-        row = ''.join(['-' for num in xrange(10)])
+        row = ''.join(['-' for num in range(10)])
         pt.add_row([row,row,row])
         for each_variable in sorted(variables_one_only):
             pt.add_row([each_variable,convert_variables_one[each_variable],''])
@@ -185,7 +190,7 @@ def main():
         for each_variable in sorted(variables_two_only):
             pt.add_row([each_variable, '',convert_variables_two[each_variable]])
             # print each_variable.ljust(30), '--'.ljust(30),convert_variables_two[each_variable].ljust(30)
-    print pt
+    print(pt)
     #print pt.get_html_string()
 if __name__ == '__main__':
     main()
